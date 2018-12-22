8ce50a3711b4c872fda2625760f0eed519ec9370
dufuqiang <1317478158@qq.com>
2018-12-22 21:59:08 +0800

    增加测试文件，未成功

---
+++ test/integration/course_query_test.rb
@@ -0,0 +1,24 @@
+require 'test_helper'
+
+class CourseQueryTest < ActionDispatch::IntegrationTest
+  # test "the truth" do
+  #   assert true
+  # end
+
+  # def setup
+  #   # @user  = users(:michael)
+  #   @user2 = users(:czj2)
+    
+  #   # @teacher    = users(:teacherluo)
+  #   # @course1 = courses(:one)
+  #   # @course2 = courses(:two)
+  #   # @course3 = courses(:three)
+  # end
+
+  # test "query course by department" do
+  # 	log_in_as(@user2)
+  #   get "/list", params: {:department => "", :type => "一级学科研讨课", :time => "", :name => ""}
+  #   assert_response 200
+  # end
+
+end
